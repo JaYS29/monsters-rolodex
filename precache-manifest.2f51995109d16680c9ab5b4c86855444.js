a30fb5f671a7e07c13a961fcd8e369f4521476fe
jays29 <juangnr@gmail.com>
2019-10-01 12:34:33 -0300

    Updates

---
+++ precache-manifest.2f51995109d16680c9ab5b4c86855444.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5980ca2a4837a6df6b87c19088c0ab7c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f575404a6427c39952af",
+    "url": "/monsters-rolodex/static/css/main.03de14f8.chunk.css"
+  },
+  {
+    "revision": "4ee53b84b1f31b69fcd8",
+    "url": "/monsters-rolodex/static/js/2.01c3c59a.chunk.js"
+  },
+  {
+    "revision": "f575404a6427c39952af",
+    "url": "/monsters-rolodex/static/js/main.13f5583f.chunk.js"
+  },
+  {
+    "revision": "61e64e548c9076aea34b",
+    "url": "/monsters-rolodex/static/js/runtime-main.c620570a.js"
+  }
+]);
